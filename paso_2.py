57e561160eb60302bf0ac7c2860577b47fb6f6df
rociomalen98 <rociomalen.98@gmail.com>
2024-10-29 19:02:14 -0300

    numeros y operaciones matematicas

---
+++ paso_2.py
@@ -0,0 +1,21 @@
+"""Crear un script de python que contenga dos variables, a y b, y
+deben asignarles los valores 7 y 3. Calcula y mostrar la suma,
+resta, multiplicación, división y módulo (residuo de una división)
+de a y b"""
+
+a = 7
+b = 3
+
+#operaciones
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b
+
+# Mostrar resultados
+print("Resultado de suma a+b:", suma)
+print("Resultado de resta a-b:", resta)
+print("Resultado de multiplicación a*b:", multiplicacion)
+print("Resultado de división a/b:", division)
+print("Resultado de módulo:", modulo)

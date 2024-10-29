01905dc96f1f86e5bdc7663f5e2f632f9e6d0d36
rociomalen98 <rociomalen.98@gmail.com>
2024-10-29 19:07:08 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,12 @@
+"""Crear un script que contenga una tupla mi_tupla con tres tipos
+diferentes de datos.
+Crea una tupla con un solo elemento e imprimir su tipo de
+dato"""
+#creando tupla con 3 tipos de datos diferentes
+mi_tupla = (27, "rocio", 1.58)
+
+# Crear una tupla con un solo elemento
+un_elemento = (5,)
+
+# Mostrar tipo de dato
+print("Tipo de dato de la tupla con un solo elemento:", type(un_elemento))

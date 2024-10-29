7539bdad47106498745399440dcf296dc0115c05
rociomalen98 <rociomalen.98@gmail.com>
2024-10-29 19:09:43 -0300

    if else

---
+++ if_else.py
@@ -0,0 +1,9 @@
+"""Escribir un programa que tome dos números y muestre el mayor
+de ellos. Utilizando unicamente la sentencia If-else."""
+
+a = 5
+b = 8
+if a > b:
+    print (a)
+else:
+    print(b)

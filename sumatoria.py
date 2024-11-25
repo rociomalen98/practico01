1e41fdbf415f5528dda738192cfc2181e9bc5a65
rociomalen98 <rociomalen.98@gmail.com>
2024-11-25 15:29:21 -0300

    sumatoria

---
+++ sumatoria.py
@@ -0,0 +1,9 @@
+"""Crear un script de python que contenga una función con el
+nombre “sumar”, reciba 2 parámetros y retorne la suma de los dos
+valores pasados como parámetros."""
+
+def sumar(x, y):
+    return x + y
+
+# probamos la funcion
+print(sumar(3, 5))
